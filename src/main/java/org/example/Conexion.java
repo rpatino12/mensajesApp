136a2b9e50fd20bd6a5b5c33ed555ccbe96ab7b9
Ricardo Patino <rpatino12@outlook.com>
2023-01-31 17:52:59 -0500

    Agregamos dependencia Maven para conectar con la base de datos,creamos clase Conexion con el metodo get_connection y en la clase Inicio nos conectamos a la base de datos

---
+++ src/main/java/org/example/Conexion.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    public Connection get_connection(){
+        Connection connection = null;
+        try{
+            // En este caso se utiliza localhost para el ejemplo, pero en un caso profesional se utiliza la url de la base de datos
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/mensajes_app","root","");
+            // Ahora validamos si efectivamente la conexion se realiza
+            if (connection != null){
+                // Si el objeto no es nulo entonces imprimimos un mensaje de conexion exitosa
+                System.out.println("Conexion exitosa!");
+            }
+        }
+        // Ahora en caso de que no se pueda generar esa conexion el bloque catch recibe la excepcion e imrpime el mensaje
+        catch (SQLException e){
+            System.out.println(e);
+        }
+        return connection;
+    }
+}
--- src/main/java/org/example/Inicio.java
+++ src/main/java/org/example/Inicio.java
@@ -1,5 +1,16 @@
 package org.example;
 
+import java.sql.Connection;
+
 public class Inicio {
-    
+    public static void main(String[] args) {
+        Conexion conexion = new Conexion();
+
+        try{
+            Connection cnx = conexion.get_connection();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
