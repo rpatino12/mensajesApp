c2fad4bb50a5ef763d7de83c77071f8370e810cb
Ricardo Patino <rpatino12@outlook.com>
2023-02-01 17:04:36 -0500

    Archivo .gitignore agregado y se agregan unos comentarios al codigo

--- src/main/java/org/example/Inicio.java
+++ src/main/java/org/example/Inicio.java
@@ -4,8 +4,9 @@ import java.sql.Connection;
 
 public class Inicio {
     public static void main(String[] args) {
+        // Creamos un objeto de tipo Conexion
         Conexion conexion = new Conexion();
-
+        // Nos conectamos a la base de datos, con el metodo get_connection del objeto conexion
         try{
             Connection cnx = conexion.get_connection();
         }
